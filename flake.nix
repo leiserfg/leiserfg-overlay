ac14eaaa0b68b0aece48841e0af6313226ae3e38
leiserfg <leiserfg@gmail.com>
2022-06-19 11:39:03 +0200

    Add packages

--- flake.nix
+++ flake.nix
@@ -2,19 +2,26 @@
     description = "My home brew packages";
     inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
 
-    outputs = { self, nixpkgs }: {
-        let  pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            overlays = [ self.overlays.default ];
-        };
+    outputs = { self, nixpkgs }:
+        let
+            pkgs = import nixpkgs {
+                system = "x86_64-linux";
+                overlays = [ self.overlays.default ];
+            };
+
 
         in {
-            overlays.default = final: prev: {
+            overlays.default = final: prev:  {
+                armourpaint = pkgs.callPackage ./pkgs/armourpaint {};
+                glslviewer = pkgs.callPackage ./pkgs/glslviewer {};
+                nsxiv-extras = pkgs.callPackage ./pkgs/nsxiv-extras {};
+
                 packages.x86_64-linux = rec {
                     inherit (pkgs)
-
+                    armourpaint
+                    glslviewer
+                    nsxiv-extras;
                 };
             };
         };
-    };
 }
---
+++ pkgs/glslviewer/default.nix
@@ -0,0 +1,40 @@
+{
+    withFFMPG ? true,
+    stdenv, cmake, ninja, lib, fetchFromGitHub, pkg-config,
+    libX11, libXrandr, libXinerama, libXcursor, libXi, libXext, libGLU, ffmpeg, ncurses
+}:
+
+stdenv.mkDerivation rec {
+  pname = "glslViewer";
+  version = "2.1.2-b5ddbb3 ";
+  src = fetchFromGitHub {
+      owner = "patriciogonzalezvivo";
+      repo = pname;
+      fetchSubmodules = true;
+      rev = "d12fff0";
+      sha256 = "sha256-qwT2fyznMkmyls4wUmBKZDIMo35qEfKjLkUYWd+UTLE=";
+  };
+  nativeBuildInputs = [cmake ninja pkg-config];
+  buildInputs = [
+      libX11
+      libXrandr
+      libXinerama
+      libXcursor
+      libXi
+      libXext
+      libGLU
+      ncurses
+  ] ++ lib.optional withFFMPG ffmpeg;
+
+  cmakeFlags = [
+      "-DCMAKE_BUILD_TYPE='Release'"
+      "-GNinja"
+  ];
+
+  meta = with lib; {
+      description = "Live GLSL coding renderer";
+      homepage = "http://patriciogonzalezvivo.com/2015/glslViewer/";
+      license = licenses.bsd3;
+      platforms = platforms.linux;
+  };
+}
