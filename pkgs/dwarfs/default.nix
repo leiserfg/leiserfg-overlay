745e077ca5882f1e95e5287d15760113b6d8c0e2
leiserfg <leiserfg@gmail.com>
2022-06-22 00:02:16 +0200

    Add dwarfs patch

--- pkgs/dwarfs/default.nix
+++ pkgs/dwarfs/default.nix
@@ -7,8 +7,25 @@
   bison,
   flex,
   ronn,
-  fuse3,
   pkg-config,
+  git,
+
+  fmt_8,
+  boost,
+  jemalloc,
+  double-conversion,
+  libiberty,
+  lz4,
+  zstd,
+  lzma,
+  openssl,
+  libunwind,
+  libdwarf,
+  fuse3,
+  glog,
+  libarchive,
+  libevent,
+
 }:
 stdenv.mkDerivation rec {
   pname = "dwarfs";
@@ -21,22 +38,43 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-bGJkgcq8JxueRTX08QpJv1A0O5wXbiIgUY7BrY0Ln/M=";
   };
   nativeBuildInputs = [
+  git
   cmake
   ninja
   bison
   flex
   ronn
-  fuse3
   pkg-config
-
   ];
-  buildInputs = [ ];
-    
-  patchPhase = ''
-    sed '31 i set(PRJ_GIT_REV  v${version})' -i cmake/version.cmake
-    sed '52 i set(PRJ_GIT_DESC v${version})' -i cmake/version.cmake
-  '';
+  buildInputs = [
+  fmt_8
+  boost
+  jemalloc
+  double-conversion
+  libiberty
+  lz4
+  lzma
+  openssl
+  libunwind
+  libdwarf
+  fuse3
+  glog
+  zstd
+  libevent
+  libarchive
+  ];
 
+  # patchPhase = ''
+  #
+  #   # insert some vals that are supposed to come from git
+  #    # sed '/\s*execute_process./,+4d' -i cmake/version.cmake
+  #    # sed '21 i set(PRJ_GIT_REV  "fake-commit")' -i cmake/version.cmake
+  #    # sed '21 i set(PRJ_GIT_BRANCH  "main")' -i cmake/version.cmake
+  #    # sed '21 i set(PRJ_GIT_DESC "v${version}")' -i cmake/version.cmake
+  #
+  #
+  # '';
+    patches = [./version.patch];
   meta = with lib; {
     description = "A fast high compression read-only file system";
     homepage = "https://github.com/mhx/dwarfs";
