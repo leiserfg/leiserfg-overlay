e5bc94288dd0f259a3d9da068976a395d0308f05
leiserfg <leiserfg@gmail.com>
2022-06-22 09:27:07 +0200

    Format

--- pkgs/dwarfs/default.nix
+++ pkgs/dwarfs/default.nix
@@ -9,7 +9,6 @@
   ronn,
   pkg-config,
   git,
-
   fmt_8,
   boost,
   jemalloc,
@@ -25,7 +24,6 @@
   glog,
   libarchive,
   libevent,
-
 }:
 stdenv.mkDerivation rec {
   pname = "dwarfs";
@@ -38,43 +36,33 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-bGJkgcq8JxueRTX08QpJv1A0O5wXbiIgUY7BrY0Ln/M=";
   };
   nativeBuildInputs = [
-  git
-  cmake
-  ninja
-  bison
-  flex
-  ronn
-  pkg-config
+    git
+    cmake
+    ninja
+    bison
+    flex
+    ronn
+    pkg-config
   ];
   buildInputs = [
-  fmt_8
-  boost
-  jemalloc
-  double-conversion
-  libiberty
-  lz4
-  lzma
-  openssl
-  libunwind
-  libdwarf
-  fuse3
-  glog
-  zstd
-  libevent
-  libarchive
+    fmt_8
+    boost
+    jemalloc
+    double-conversion
+    libiberty
+    lz4
+    lzma
+    openssl
+    libunwind
+    libdwarf
+    fuse3
+    glog
+    zstd
+    libevent
+    libarchive
   ];
 
-  # patchPhase = ''
-  #
-  #   # insert some vals that are supposed to come from git
-  #    # sed '/\s*execute_process./,+4d' -i cmake/version.cmake
-  #    # sed '21 i set(PRJ_GIT_REV  "fake-commit")' -i cmake/version.cmake
-  #    # sed '21 i set(PRJ_GIT_BRANCH  "main")' -i cmake/version.cmake
-  #    # sed '21 i set(PRJ_GIT_DESC "v${version}")' -i cmake/version.cmake
-  #
-  #
-  # '';
-    patches = [./version.patch];
+  patches = [./version.patch];
   meta = with lib; {
     description = "A fast high compression read-only file system";
     homepage = "https://github.com/mhx/dwarfs";
--- pkgs/material-maker/default.nix
+++ pkgs/material-maker/default.nix
@@ -5,7 +5,6 @@
   autoPatchelfHook,
   makeDesktopItem,
   copyDesktopItems,
-
   libX11,
   libXrandr,
   libXinerama,
@@ -13,62 +12,61 @@
   libXi,
   libXext,
   libGLU,
-  libXrender
-}:
-let 
-    _tr = text : (builtins.replaceStrings ["." "-"] ["_" "_"] text);
+  libXrender,
+}: let
+  _tr = text: (builtins.replaceStrings ["." "-"] ["_" "_"] text);
 in
-stdenv.mkDerivation rec {
-  pname = "material-maker";
-  version = "0.99";
+  stdenv.mkDerivation rec {
+    pname = "material-maker";
+    version = "0.99";
 
-  src = fetchurl {
-    url="https://github.com/RodZill4/${pname}/releases/download/${version}/${_tr pname}_${_tr version}_linux.tar.gz";
-    sha256 = "1gqb095dn6agqafnr53lya91jh20xlgdfp5cg5l72a6rps1i8j47";
-  };
-  desktopItems = [
-    (makeDesktopItem {
-      desktopName = "MaterialMaker";
-      exec = "material_maker";
-      name = "MaterialMaker";
-      icon = "material_maker";
-    })
-  ];
+    src = fetchurl {
+      url = "https://github.com/RodZill4/${pname}/releases/download/${version}/${_tr pname}_${_tr version}_linux.tar.gz";
+      sha256 = "1gqb095dn6agqafnr53lya91jh20xlgdfp5cg5l72a6rps1i8j47";
+    };
+    desktopItems = [
+      (makeDesktopItem {
+        desktopName = "MaterialMaker";
+        exec = "material_maker";
+        name = "MaterialMaker";
+        icon = "material_maker";
+      })
+    ];
 
-buildInputs = [
-  libX11
-  libXrandr
-  libXinerama
-  libXcursor
-  libXi
-  libXext
-  libGLU
-  libXrender
-  ];
+    buildInputs = [
+      libX11
+      libXrandr
+      libXinerama
+      libXcursor
+      libXi
+      libXext
+      libGLU
+      libXrender
+    ];
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    copyDesktopItems
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+      copyDesktopItems
+    ];
 
-  installPhase = ''
-    mkdir -p $out/bin/ $out/share/icons/hicolor/256x256/apps/
-    cp -r . $out/material_maker/
+    installPhase = ''
+      mkdir -p $out/bin/ $out/share/icons/hicolor/256x256/apps/
+      cp -r . $out/material_maker/
 
-    cat <<-EOF > $out/bin/material_maker
-    #!/usr/bin/env bash
-    $out/material_maker/material_maker.x86_64
-    EOF
+      cat <<-EOF > $out/bin/material_maker
+      #!/usr/bin/env bash
+      $out/material_maker/material_maker.x86_64
+      EOF
 
-    chmod +x $out/bin/material_maker
-    cp $out/material_maker/examples/mm_icon.png   $out/share/icons/hicolor/256x256/apps/material_maker.png
-    copyDesktopItems
-  '';
+      chmod +x $out/bin/material_maker
+      cp $out/material_maker/examples/mm_icon.png   $out/share/icons/hicolor/256x256/apps/material_maker.png
+      copyDesktopItems
+    '';
 
-  meta = with lib; {
-    description = "This is a tool based on Godot Engine that can be used to create textures procedurally and paint 3D models.";
-    homepage = "https://www.materialmaker.org/";
-    license = licenses.mit;
-    platforms = platforms.unix;
-  };
-}
+    meta = with lib; {
+      description = "This is a tool based on Godot Engine that can be used to create textures procedurally and paint 3D models.";
+      homepage = "https://www.materialmaker.org/";
+      license = licenses.mit;
+      platforms = platforms.unix;
+    };
+  }
