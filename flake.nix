1fe4b44dbf6189b976a3f19f6976f425548e3240
leiserfg <leiserfg@gmail.com>
2022-07-03 15:34:40 +0200

    Add sha256 for darktable

--- flake.nix
+++ flake.nix
@@ -17,8 +17,14 @@
       nsxiv-extras = pkgs.callPackage ./pkgs/nsxiv-extras {};
       material-maker = pkgs.callPackage ./pkgs/material-maker {};
       dwarfs = pkgs.callPackage ./pkgs/dwarfs {};
-      wasm2luajit  = pkgs.callPackage ./pkgs/wasm2luajit {};
-      darktable_4 = prev.darktable.overrideAttrs (old: rec {version="4.0.0";});
+      wasm2luajit = pkgs.callPackage ./pkgs/wasm2luajit {};
+      darktable = (prev.darktable.overrideAttrs (old: rec {
+        version = "4.0.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/darktable-org/darktable/releases/download/release-${version}/darktable-${version}.tar.xz";
+          sha256 = "0bfcag6bj5vcmg4z4xjirs43iafcx89al6jl41i5mrhpjzszh5hl";
+        };
+      })).override {lua=pkgs.lua5_4;};
     };
     packages.x86_64-linux = rec {
       inherit
@@ -28,9 +34,10 @@
         nsxiv-extras
         material-maker
         dwarfs
-        darktable_4
-        wasm2luajit ;
-        default = dwarfs;
+        darktable
+        wasm2luajit
+        ;
+      default = dwarfs;
     };
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
--- pkgs/wasm2luajit/default.nix
+++ pkgs/wasm2luajit/default.nix
@@ -3,28 +3,29 @@
   stdenv,
   fetchurl,
   autoPatchelfHook,
-}: stdenv.mkDerivation rec {
-    pname = "wasm2luajit";
-    version = "0.11.0";
-    src = fetchurl {
-      url = "https://github.com/Rerumu/Wasynth/releases/download/v${version}/wasm2luajit";
-      sha256 = "sha256-QR8cfJGEnqmwNwPW6v8a+KW5NSsCg6BqdZHf5eyygak=";
-    };
-    nativeBuildInputs = [
-      autoPatchelfHook
-    ];
-    # unpackCmd = "cp";
-    sourceRoot = ".";
-    dontUnpack = true;
+}:
+stdenv.mkDerivation rec {
+  pname = "wasm2luajit";
+  version = "0.11.0";
+  src = fetchurl {
+    url = "https://github.com/Rerumu/Wasynth/releases/download/v${version}/wasm2luajit";
+    sha256 = "sha256-QR8cfJGEnqmwNwPW6v8a+KW5NSsCg6BqdZHf5eyygak=";
+  };
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+  # unpackCmd = "cp";
+  sourceRoot = ".";
+  dontUnpack = true;
 
-    installPhase = ''
-        install -m755 -D ${src} $out/bin/wasm2luajit
-    '';
+  installPhase = ''
+    install -m755 -D ${src} $out/bin/wasm2luajit
+  '';
 
-    meta = with lib; {
-      description = "Translate wasm to lua";
-      homepage = "https://github.com/Rerumu/Wasynth";
-      license = licenses.gpl3;
-      platforms = platforms.unix;
-    };
-  }
+  meta = with lib; {
+    description = "Translate wasm to lua";
+    homepage = "https://github.com/Rerumu/Wasynth";
+    license = licenses.gpl3;
+    platforms = platforms.unix;
+  };
+}
