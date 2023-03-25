b8154287323993b7472503da2a2db9f6277a741b
leiserfg <leiserfg@gmail.com>
2023-03-25 09:20:32 +0100

    Update llama-rs

--- pkgs/llama-rs/default.nix
+++ pkgs/llama-rs/default.nix
@@ -14,12 +14,12 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "setzer22";
     repo = pname;
-    rev = "4a207f0";
-    sha256 = "sha256-Qhs6Ap2A53wPJLvGaEfZWIdHiqdDQRokC8k/iFD5C0k=";
+    rev = "91afe67";
+    sha256 = "sha256-E2BHSdehY/R1S0IiobOcz1QRs77fDWgD1aRgd0RsEFE=";
   };
   LIBCLANG_PATH = "${libclang.lib}/lib";
   BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${libclang.lib}/lib/clang/${lib.getVersion clang}/include";
-  cargoSha256 = "sha256-fTdcy2icxe9RmXFNBpSGhM2AwnLklZ4v/wdabEIMNtk=";
+  cargoSha256 = "sha256-lKRduO+MFmW1axqF/3U30R0KThVEX/stoItugKOO/XU=";
 
   meta = with lib; {
     description = "Do the LLaMA thing, but now in Rust crabrocketllama";
