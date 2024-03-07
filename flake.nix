60bc588216bf92cb8c4f52143dfc51fd2ad6f1bc
leiserfg <leiserfg@gmail.com>
2024-03-07 20:29:11 +0100

    last yuzu :'(

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "My home-brew packages";
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/b8697e57f10292a6165a20f03d2f42920dfaf973";
 
   outputs = {
     self,
@@ -15,68 +14,19 @@
   in {
     overlays.default = final: prev: {
       glslviewer = pkgs.callPackage ./pkgs/glslviewer {};
-      nsxiv-extras = pkgs.callPackage ./pkgs/nsxiv-extras {};
       material-maker = pkgs.callPackage ./pkgs/material-maker {};
-      dwarfs = pkgs.callPackage ./pkgs/dwarfs {};
-      wasm2luajit = pkgs.callPackage ./pkgs/wasm2luajit {};
       doggo = pkgs.callPackage ./pkgs/doggo {};
-      yuzu = pkgs.callPackage ./pkgs/yuzu {branch = "early-access";};
-      # pylyzer = pkgs.callPackage ./pkgs/pylyzer {
-      #   rustPlatform = pkgs.makeRustPlatform {
-      #     cargo = fenix.packages.x86_64-linux.minimal.toolchain;
-      #     rustc = fenix.packages.x86_64-linux.minimal.toolchain;
-      #   };
-      # };
-
-      awesome = prev.awesome.overrideAttrs (old: rec {
-        version = "4.4.0.alpha-lj";
-        patches = [];
-        src = pkgs.fetchFromGitHub {
-          owner = "awesomewm";
-          repo = "awesome";
-          rev = "9ca7bb4";
-          sha256 = "sha256-RRpwAIYNLkovXI0y/eXO9uRDqB4qQcXlnYYUCEmx/EA=";
-        };
-      });
-      pasystray = prev.pasystray.overrideAttrs (old: rec {
-        pname = "pasystray";
-        version = "0.8.2";
-        src = pkgs.fetchFromGitHub {
-          owner = "christophgysin";
-          repo = "pasystray";
-          rev = version;
-          sha256 = "sha256-QaTQ8yUviJaFEQaQm2vYAUngqHliKe8TDYqfWt1Nx/0=";
-        };
-      });
-      /*
-      .override {lua = pkgs.luajit;};
-      */
-      pmenu = pkgs.callPackage ./pkgs/pmenu {};
-      ansel = pkgs.callPackage ./pkgs/ansel {};
-      inkscape = pkgs.callPackage ./pkgs/inkscape {};
-      lib2geom = pkgs.callPackage ./pkgs/lib2geom {};
     };
 
     packages.x86_64-linux = rec {
       inherit
         (pkgs)
-        pmenu
-        # armourpaint
-        lib2geom
         glslviewer
-        nsxiv-extras
         material-maker
-        dwarfs
-        awesome
-        wasm2luajit
         doggo
-        git-branchless
-        ansel
-        pasystray
-        yuzu
-        inkscape
+        yuzu-early-access
         ;
-      default = yuzu;
+      default = yuzu-early-access;
     };
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
