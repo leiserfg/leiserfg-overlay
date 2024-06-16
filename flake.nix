0d59e9a6ae2f892622eb208fbef7688625408e17
leiserfg <leiserfg@gmail.com>
2024-06-16 11:20:16 +0200

    Sun Jun 16 11:20:16 AM CEST 2024

--- flake.nix
+++ flake.nix
@@ -1,11 +1,7 @@
 {
   description = "My home-brew packages";
 
-  # inputs.nixpkgs.url = "github:nixos/nixpkgs/b8697e57f10292a6165a20f03d2f42920dfaf973";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-  # inputs.suyu-flake.url = "github:leiserfg/suyu-flake/rebrand";
-  # inputs.suyu-flake.url = "github:Noodlez1232/suyu-flake";
-  # inputs.suyu-flake.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = {
     self,
@@ -30,7 +26,6 @@
       nx_tzdb = pkgs.callPackage ./pkgs/torzu/nx_tzdb.nix {};
       compat-list = pkgs.callPackage ./pkgs/torzu/compat-list.nix {};
       torzu = pkgs.callPackage ./pkgs/torzu/torzu.nix suyu-deps;
-      # sudachi = pkgs.callPackage ./pkgs/torzu/sudachi.nix suyu-deps;
     };
 
     packages.x86_64-linux = rec {
