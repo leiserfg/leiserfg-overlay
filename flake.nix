68567bca5441452abe7441ef4fa18cda25753d07
leiserfg <leiserfg@gmail.com>
2025-09-09 21:40:47 +0200

    Add pixieditor

--- flake.nix
+++ flake.nix
@@ -34,10 +34,11 @@
         compat-list = pkgs.callPackage ./pkgs/torzu/compat-list.nix { };
         torzu = pkgs.callPackage ./pkgs/torzu/torzu.nix { };
         # eden-emu = pkgs.callPackage ./pkgs/torzu/eden.nix suyu-deps; # WIP
-        eden-emu = pkgs.kdePackages.callPackage ./pkgs/torzu/eden_appimage.nix {}; # WIP
+        eden-emu = pkgs.kdePackages.callPackage ./pkgs/torzu/eden_appimage.nix { }; # WIP
         pyglossary = pkgs.callPackage ./pkgs/pyglossary { };
         # wl_shimeji = pkgs.callPackage ./pkgs/wl_shimeji { };
         friction-graphics = pkgs.callPackage ./pkgs/friction-graphics { };
+        pixieditor = pkgs.callPackage ./pkgs/pixieditor/package.nix { };
       };
 
       packages.x86_64-linux = rec {
@@ -55,6 +56,7 @@
           pyglossary
           jpegli
           # wl_shimeji
+          pixieditor
           ;
         default = glslviewer;
       };
