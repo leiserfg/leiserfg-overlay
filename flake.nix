c4998801ce5b24cda86197490c04cbacf8406246
leiserfg <leiserfg@gmail.com>
2025-11-12 21:41:24 +0100

    kitty

--- flake.nix
+++ flake.nix
@@ -18,9 +18,9 @@
         overlays = [ self.overlays.default ];
       };
 
-      suyu-deps = pkgs.kdePackages // {
-        inherit (self.packages.${system}) nx_tzdb compat-list;
-      };
+      # suyu-deps = pkgs.kdePackages // {
+      #   inherit (self.packages.${system}) nx_tzdb compat-list;
+      # };
     in
     {
       overlays.default = final: prev: {
@@ -29,30 +29,35 @@
         };
         vicinae = vicinae.packages.x86_64-linux.default;
         jpegli = pkgs.callPackage ./pkgs/jpgli { };
-        nx_tzdb = pkgs.callPackage ./pkgs/torzu/nx_tzdb.nix { };
-        compat-list = pkgs.callPackage ./pkgs/torzu/compat-list.nix { };
+        # nx_tzdb = pkgs.callPackage ./pkgs/torzu/nx_tzdb.nix { };
+        # compat-list = pkgs.callPackage ./pkgs/torzu/compat-list.nix { };
         # torzu = pkgs.callPackage ./pkgs/torzu/torzu.nix { };
 
         # eden-emu = pkgs.callPackage ./pkgs/torzu/eden.nix suyu-deps; # WIP
         eden-emu = pkgs.kdePackages.callPackage ./pkgs/torzu/eden_appimage.nix { };
-        pyglossary = pkgs.callPackage ./pkgs/pyglossary { };
+        # pyglossary = pkgs.callPackage ./pkgs/pyglossary { };
         # friction-graphics = pkgs.libsForQt5.callPackage ./pkgs/friction-graphics/friction_appimagen.nix { };
         # friction-graphics = pkgs.callPackage ./pkgs/friction-graphics { };
         wl_shimeji = pkgs.callPackage ./pkgs/wl_shimeji { };
         # material-maker = pkgs.callPackage ./pkgs/material-maker { };
         pixieditor = pkgs.callPackage ./pkgs/pixieditor/package.nix { };
+        # kitty = pkgs.callPackage ./pkgs/kitty/package.nix {
+        #   inherit (pkgs.darwin) autoSignDarwinBinariesHook;
+        # };
       };
 
       packages.x86_64-linux = rec {
         inherit (pkgs)
+          # kitty
+
           # friction-graphics
           # material-maker
           eden-emu
           glslviewer
           # torzu
-          nx_tzdb
-          compat-list
-          pyglossary
+          # nx_tzdb
+          # compat-list
+          # pyglossary
           jpegli
           pixieditor
           wl_shimeji
